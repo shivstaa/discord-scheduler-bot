eb84f098a7d5407dfcbac9e83609e343a3f71ea0
Bura-bura <ho.bryant.2021@gmail.com>
2023-11-16 01:36:29 -0700

    asyncpg implementation

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import asyncio
 import discord
+import asyncpg
 from discord import app_commands
 from discord.ext import commands
 import os
@@ -6,17 +8,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 intents = discord.Intents.all()
+
 # intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-
     # verifies # of commands that are functional on Discord
     try: 
+        bot.pool = await asyncpg.create_pool(
+            host=os.getenv("HOST"),
+            database=os.getenv("DATABASE"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD")
+        )
         synced = await bot.tree.sync()
         print(f"Synced {len(synced)} command(s)")
+        
     except Exception as e:
         print(e)
 
@@ -32,5 +41,22 @@ async def create_private_event(interaction: discord.Interaction, event_name: str
 async def create_group_event(interaction: discord.Interaction, event_name: str, event_start_date: str, event_end_date: str, event_start_time: str, event_end_time: str):
     await interaction.response.send_message(f"{interaction.user.name} {event_name} created from {event_start_date} at {event_start_time} to {event_end_date} at {event_end_time}")
 
+#list out all events (currently includes private.)
+@bot.tree.command(name="show_events")
+async def show_events(interaction: discord.Interaction):
+    async with bot.pool.acquire() as conn:  # Acquire a connection from the pool
+        uiud = interaction.user.name  # Using the user's name as the UIUD
+        rows = await conn.fetch(
+            "SELECT meetingname, location, timestart, timeend FROM event WHERE uiud = $1", uiud
+        )
+        if rows:
+            response = "Here are your events:\n" + "\n".join(
+                f"{row['meetingname']} at {row['location']}, From {row['timestart']} To {row['timeend']}"
+                for row in rows
+            )
+        else:
+            response = "No events."
+        await interaction.response.send_message(response)
+    
 
 bot.run(os.getenv("DISCORD_TOKEN"))
