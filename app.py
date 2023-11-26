91c755cb684792c93924b6f65c4c306d626709d0
Bura-bura <bryantho@asu.edu>
2023-11-26 01:36:02 -0700

    implementations of both types of inserts, as well as views for both types of events.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 import asyncpg
+from datetime import datetime
 from discord import app_commands
 from discord.ext import commands
 import os
@@ -31,32 +32,180 @@ async def on_ready():
 
 # create private event
 @bot.tree.command(name="create_private_event")
-@app_commands.describe(event_name="Event name", event_start_date="Event Start Date", event_end_date="Event End Date", event_start_time="Event Start Time", event_end_time="Event End Time")
-async def create_private_event(interaction: discord.Interaction, event_name: str, event_start_date: str, event_end_date: str, event_start_time: str, event_end_time: str):
-    await interaction.response.send_message(f"{interaction.user.name} {event_name} created from {event_start_date} at {event_start_time} to {event_end_date} at {event_end_time}")
+@app_commands.describe(
+    event_name="Event name", 
+    event_start_date="Event Start Date", 
+    event_end_date="Event End Date", 
+    event_start_time="Event Start Time", 
+    event_end_time="Event End Time",
+    event_location="Event Location"
+)
+async def create_private_event(
+    interaction: discord.Interaction, 
+    event_name: str, 
+    event_location: str, 
+    event_start_date: str, 
+    event_end_date: str, 
+    event_start_time: str, 
+    event_end_time: str
+):
+    uiud = str(interaction.user.id)
+    user_name = interaction.user.name
+    event_start = f"{event_start_date} {event_start_time}"
+    event_end = f"{event_end_date} {event_end_time}"
+    event_start = datetime.strptime(f"{event_start_date} {event_start_time}", "%Y-%m-%d %H:%M:%S")
+    event_end = datetime.strptime(f"{event_end_date} {event_end_time}", "%Y-%m-%d %H:%M:%S")
+    async with bot.pool.acquire() as conn:
+
+        user = await conn.fetchrow("SELECT * FROM \"user\" WHERE uiud = $1", uiud)
+        if user is None:
+            await conn.execute("INSERT INTO \"user\" (uiud, name) VALUES ($1, $2)", uiud, user_name)
+            
+        overlap = await conn.fetchrow(
+            """
+            SELECT * FROM event
+            WHERE uiud = $1
+            AND timestart < $3 
+            AND timeend > $2
+            """,
+            uiud, event_start, event_end
+        )
+        if overlap:
+            await interaction.response.send_message(
+                f"{interaction.user.mention}, there is an overlapping event."
+            )
+            return
+        
+        
+        eid = await conn.fetchval(
+            "INSERT INTO event (uiud, meetingname, location, timestart, timeend) VALUES ($1, $2, $3, $4, $5) RETURNING eid",
+            uiud, event_name, event_location, event_start, event_end
+        )
+        if eid:
+            await interaction.response.send_message(
+                f"{interaction.user.mention}, {event_name} at {event_location} has been scheduled for {event_start_date} from {event_start_time} to {event_end_time}.", ephemeral=True
+            )
+        else:
+            await interaction.response.send_message(
+                f"{interaction.user.mention}, there was an issue creating the event.", ephemeral=True
+            )
 
 # create group event
 @bot.tree.command(name="create_group_event")
-@app_commands.describe(event_name="Event name", event_start_date="Event Start Date", event_end_date="Event End Date", event_start_time="Event Start Time", event_end_time="Event End Time")
-async def create_group_event(interaction: discord.Interaction, event_name: str, event_start_date: str, event_end_date: str, event_start_time: str, event_end_time: str):
-    await interaction.response.send_message(f"{interaction.user.name} {event_name} created from {event_start_date} at {event_start_time} to {event_end_date} at {event_end_time}")
+@app_commands.describe(
+    event_name="Event name", 
+    event_start_date="Event Start Date", 
+    event_end_date="Event End Date", 
+    event_start_time="Event Start Time", 
+    event_end_time="Event End Time",
+    event_location="Event Location"
+)
+async def create_group_event(
+    interaction: discord.Interaction, 
+    event_name: str, 
+    event_location: str, 
+    event_start_date: str, 
+    event_end_date: str, 
+    event_start_time: str, 
+    event_end_time: str
+):
+    if interaction.guild is None:
+        await interaction.response.send_message("This is for creating server events!", ephemeral=True)
+        return
 
-#list out all events (currently includes private.)
+    gid = interaction.guild_id
+    guild_name = interaction.guild.name
+    uiud = str(interaction.user.id)
+    user_name = interaction.user.name
+    event_start = datetime.strptime(f"{event_start_date} {event_start_time}", "%Y-%m-%d %H:%M:%S")
+    event_end = datetime.strptime(f"{event_end_date} {event_end_time}", "%Y-%m-%d %H:%M:%S")
+    
+    async with bot.pool.acquire() as conn:
+        # Check if the user exists
+        user = await conn.fetchrow("SELECT * FROM \"user\" WHERE uiud = $1", uiud)
+        if user is None:
+            await conn.execute("INSERT INTO \"user\" (uiud, name) VALUES ($1, $2)", uiud, user_name)
+        
+        # Check if the guild exists in the "group" table
+        group = await conn.fetchrow("SELECT * FROM \"group\" WHERE gid = $1", gid)
+        if group is None:
+            await conn.execute("INSERT INTO \"group\" (gid, groupname) VALUES ($1, $2)", gid, guild_name)
+            
+        usergroup = await conn.fetchrow(
+            "SELECT * FROM usergroup WHERE uiud = $1 AND gid = $2", 
+            uiud, gid
+        )
+        if usergroup is None:
+            # Associate the user with the guild if not already associated
+            await conn.execute(
+                "INSERT INTO usergroup (uiud, gid) VALUES ($1, $2)", 
+                uiud, gid
+            )
+        
+        # Check for overlapping events
+        overlap = await conn.fetchrow(
+            "SELECT * FROM event WHERE uiud = $1 AND timestart < $3 AND timeend > $2",
+            uiud, event_start, event_end
+        )
+        if overlap:
+            await interaction.response.send_message(
+                f"{interaction.user.mention}, there is an overlapping event.", ephemeral=True
+            )
+            return
+        
+
+        eid = await conn.fetchval(
+            "INSERT INTO event (uiud, gid, meetingname, location, timestart, timeend) VALUES ($1, $2, $3, $4, $5, $6) RETURNING eid",
+            uiud, gid, event_name, event_location, event_start, event_end
+        )
+        if eid:
+            await interaction.response.send_message(
+                f"{interaction.user.mention}, {event_name} at {event_location} has been scheduled for {event_start_date} from {event_start_time} to {event_end_time} for the group {guild_name}."
+            )
+        else:
+            await interaction.response.send_message(
+                f"{interaction.user.mention}, there was an issue creating the group event.",
+                ephemeral=True
+            )
+            
+
+#list out all events (outputs only to original user)
 @bot.tree.command(name="show_events")
 async def show_events(interaction: discord.Interaction):
-    async with bot.pool.acquire() as conn:  # Acquire a connection from the pool
-        uiud = interaction.user.name  # Using the user's name as the UIUD
+    async with bot.pool.acquire() as conn:
+        uiud = str(interaction.user.id)
         rows = await conn.fetch(
             "SELECT meetingname, location, timestart, timeend FROM event WHERE uiud = $1", uiud
         )
         if rows:
             response = "Here are your events:\n" + "\n".join(
-                f"{row['meetingname']} at {row['location']}, From {row['timestart']} To {row['timeend']}"
+                f"{row['meetingname']} at {row['location']}, from {row['timestart']} to {row['timeend']}."
                 for row in rows
             )
         else:
             response = "No events."
+        await interaction.response.send_message(response, ephemeral=True)
+        
+@bot.tree.command(name="show_server_events")
+async def show_server_events(interaction: discord.Interaction):
+    gid = interaction.guild_id 
+    if gid is None:
+        await interaction.response.send_message("This command can only be used in a server.", ephemeral=True)
+        return
+    async with bot.pool.acquire() as conn:
+        rows = await conn.fetch(
+            "SELECT meetingname, location, timestart, timeend FROM event WHERE gid = $1", gid
+        )
+
+        if rows:
+            response = f"Here are {interaction.guild.name}'s events:\n" + "\n".join(
+                f"{row['meetingname']} at {row['location']}, from {row['timestart']} to {row['timeend']}."
+                for row in rows
+            )
+        else:
+            response = "No server events."
         await interaction.response.send_message(response)
+
     
 
 bot.run(os.getenv("DISCORD_TOKEN"))
